2f0ce71c3d0a52b579baaf41f90cf4c9e0945d36
Shunichi Shinohara <shino@shiguredo.jp>
2020-07-15 12:19:08 +0900

    use v2

--- src/rebar3_static_resource.erl
+++ src/rebar3_static_resource.erl
@@ -2,31 +2,37 @@
 
 -behaviour(rebar_resource_v2).
 
--export([lock/2,
-         download/3,
+-export([init/2,
+         lock/2,
+         download/4,
          needs_update/2,
-         make_vsn/1]).
+         make_vsn/2]).
 
-lock(Dir, Source) ->
-    rebar_git_resource:lock(Dir, to_normal_resource(Source)).
+init(Type, State) ->
+    rebar_api:warn("rebar3_static_resource:init(): Type=~p", [Type]),
+    rebar_git_resource:init(Type, State).
 
-download(Dir, Source, State) ->
-    rebar_api:warn("Source=~p", [Source]),
-    case rebar_git_resource:download(Dir, to_normal_resource(Source), State) of
+lock(AppInfo, ResourceState) ->
+    rebar_git_resource:lock(AppInfo, ResourceState).
+
+download(Dir, AppInfo, ResourceState, State) -
+    rebar_api:warn("rebar3_static_resource:download(): AppInfo=~p", [AppInfo]),
+    rebar_api:warn("rebar3_static_resource:download(): ResourceState=~p", [ResourceState]),
+    case rebar_git_resource:download(Dir, AppInfo, ResourceState, State) of
         {error, Reason} ->
             {error, Reason};
         ok ->
-            write_app_file(Dir, Source);
+            write_app_file(Dir, AppInfo);
         {ok, _} ->
-            write_app_file(Dir, Source)
+            write_app_file(Dir, AppInfo)
     end.
 
 
-needs_update(Dir, Source) ->
-    rebar_git_resource:make_vsn(Dir, to_normal_resource(Source)).
+needs_update(AppInfo, ResourceState) ->
+    rebar_git_resource:make_vsn(AppInfo, to_normal_resource(ResourceState)).
 
-make_vsn(Dir) ->
-    rebar_git_resource:make_vsn(Dir).
+make_vsn(AppInfo, ResourceState) ->
+    rebar_git_resource:make_vsn(AppInfo, ResourceState).
 
 %% private
 
@@ -34,10 +40,10 @@ to_normal_resource({static, Repo, Vsn}) ->
     {git, Repo, Vsn}.
 
 write_app_file(Dir, {_, Repo, _}) ->
-    rebar_api:warn("Dir=~p", [Dir]),
+    rebar_api:warn("rebar3_static_resource:write_app_file(): Dir=~p", [Dir]),
     AppName = lists:flatten(string:replace(filename:basename(Repo, ".git"), "-", "_", all)),
     AppFilePath = filename:join([Dir, "src", AppName ++ ".app.src"]),
-    rebar_api:warn("AppFilePath=~p", [AppFilePath]),
+    rebar_api:warn("rebar3_static_resource:write_app_file(): AppFilePath=~p", [AppFilePath]),
     ok = filelib:ensure_dir(AppFilePath),
     Content = <<"{application, ", (iolist_to_binary(AppName))/binary, ",\n",
                 "  [{description, \"fake static app\"},\n",
@@ -45,6 +51,6 @@ write_app_file(Dir, {_, Repo, _}) ->
                 "   {registered, []},\n",
                 "   {applications, [kernel, stdlib]},\n",
                 "   {modules, []}]}.\n">>,
-    rebar_api:warn("AppFile content=~p", [Content]),
+    rebar_api:warn("rebar3_static_resource:write_app_file(): AppFile content\n~ts", [Content]),
     ok = file:write_file(AppFilePath, Content),
     ok.
